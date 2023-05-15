764484a65c8a91e3e88ceb7879b985ef237f2535
Divubhuva <divubhuva@gmail.com>
2023-05-14 22:50:31 -0700

    Provide the margin according to window.

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Dimensions, StyleSheet} from 'react-native';
 import MenuScreen from './Components/MenuScreen ';
+
+var deviceWidth = Dimensions.get('screen').width;
+var deviceHeight = Dimensions.get('screen').height;
 
 const App = () => {
   return (
-    <View style={{flex: 1}}>
+    <View style={styles.MainPageStyle}>
       <MenuScreen />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  MainPageStyle: {
+    flex: 1,
+    marginTop: deviceHeight * 0.05,
+    marginBottom: deviceHeight * 0.02,
+    marginLeft: deviceWidth * 0.02,
+    marginRight: deviceWidth * 0.02,
+  },
+});
 
 export default App;
